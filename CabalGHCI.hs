31c5c23d1237170a8d32f480c407fdc6faf7810e
Herbert Valerio Riedel <hvr@gnu.org>
2013-05-25 17:15:49 +0200

    sync up with v0.2.1 release
    as available on Hackage
    http://hackage.haskell.org/packages/archive/cabal-ghci/0.2.1/cabal-ghci-0.2.1.tar.gz
    
    (alas http://code.atnnn.com/darcs/cabal-ghci/ is down currently)

--- CabalGHCI.hs
+++ CabalGHCI.hs
@@ -5,9 +5,17 @@ import Distribution.Dev.Interactive
 import System.Environment
 import System.Process
 
+opt0 opt args = f [] args
+     where f r (x:xs)
+             | x == ("--cabal-ghci-" ++ opt) = (reverse r ++ xs, True)
+             | otherwise = f (x:r) xs
+           f _ _ = (args, False)
+
 main = do
   args ← getArgs
-  withOpts args putStrLn $ 
-        \opts → do
+  let (args', pretend) = opt0 "print-ghci-args" args
+  withOpts args' putStrLn $
+    if pretend then mapM_ putStrLn
+    else \opts → do
           putStrLn $ "Executing ghci with the following options: " ++ unwords opts
           rawSystem "ghci" opts >> return ()
